55d7544897d85d718839ec463abc0ada03a79903
nathan shearer <help@rapidstartup.io>
2023-08-01 14:16:51 +0800

    add plan banner

--- resources/views/themes/tailwind/partials/plans.blade.php
+++ resources/views/themes/tailwind/partials/plans.blade.php
@@ -5,9 +5,10 @@
         <div class="w-full max-w-md px-0 mx-auto mb-6 lg:w-1/3 lg:px-3 lg:mb-0">
             <div class="relative flex flex-col h-full mb-10 bg-white dark:bg-gray-800 bg-opacity-15 border border-gray-200 rounded-lg shadow-xl border-b-none sm:mb-0">
                 <div class="px-10 pt-7">
-                    <div class="absolute right-0 inline-block mr-6 transform">
-                        <h2 class="relative z-20 w-full h-full px-2 py-1 text-xs font-bold leading-tight tracking-wide text-center uppercase bg-white border-2 @if($plan->default){{ 'border-none text-white bg-indigo-400' }}@else{{ 'hidden border-gray-900 text-gray-800' }}@endif rounded transform rotate-45">Best value</h2>
-
+                    <div class="absolute right-0 inline-block transform">
+                        <h2 class="relative z-20 w-full h-full px-2 py-1 text-xs leading-tight tracking-wide text-center uppercase bg-white border-2 @if($plan->default){{ 'border-none dark:text-white bg-indigo-700' }}@else{{ 'hidden border-gray-900 text-gray-800' }}@endif rounded transform rotate-45">Best value</h2>
+                        <span class="absolute -top-7 left-1 w-4 h-4 bg-indigo-700 transform p-2" style="z-index: -1;"></span>
+                        <span class="absolute -bottom-7 right-1 w-5 h-4 bg-indigo-700 transform" style="z-index: -1;"></span>
                     </div>
                 </div>
 
