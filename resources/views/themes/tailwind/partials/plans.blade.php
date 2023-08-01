e34bd0e52c5e0ece9d26773604596064e8d98951
nathan shearer <help@rapidstartup.io>
2023-08-01 22:54:16 +0800

    update plans

--- resources/views/themes/tailwind/partials/plans.blade.php
+++ resources/views/themes/tailwind/partials/plans.blade.php
@@ -3,7 +3,7 @@
         @php $features = explode(',', $plan->features); @endphp
 
         <div class="w-full max-w-md px-0 mx-auto mb-6 lg:w-1/4 lg:px-3 lg:mb-0">
-            <div class="relative flex flex-col h-full mb-10 bg-white dark:bg-gray-800 bg-opacity-15 border border-gray-200 rounded-lg shadow-xl border-b-none sm:mb-0">
+            <div class="relative flex flex-col h-full mb-10 bg-white dark:bg-gray-800 bg-opacity-15 border @if($plan->default){{ 'border-indigo-700' }}@else{{ 'border-none' }}@endif rounded-lg shadow-xl border-b-none sm:mb-0">
                 <div class="px-4 pt-7">
                     <div class="absolute right-0 inline-block transform">
                         <h2 class="relative z-20 w-full h-full px-2 py-1 text-xs leading-tight tracking-wide text-center uppercase border-2 @if($plan->default){{ 'border-none dark:text-white bg-indigo-700' }}@else{{ 'hidden border-gray-900 text-gray-800' }}@endif rounded transform rotate-45">Best value</h2>
@@ -25,29 +25,8 @@
                     <span class="text-md font-bold text-gray-500">per month</span>
                 </div>
 
-                <div class="relative px-4 pt-0 pb-12 mt-auto text-gray-700 rounded-b-lg">
-
-                    <ul class="flex flex-col space-y-2.5">
-                        @foreach($features as $feature)
-                            <li class="relative">
-                                    <span class="flex items-center">
-                                    <svg class="w-4 h-4 mr-3 text-gray-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z"></path>
-                                        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" transform="translate(4, 0)"></path>
-                                    </svg>
-                                    <span>
-                                        {{ $feature }}
-                                    </span>
-                                </span>
-                            </li>
-                        @endforeach
-                    </ul>
-
-
-                </div>
-
                 <div class="relative inline-flex">
-                        <div data-plan="{{ $plan->plan_id }}" class="inline-flex items-center justify-center mx-auto w-3/4 px-4 py-4 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-wave-600 to-indigo-500 hover:from-wave-500 hover:to-indigo-400' }}@else{{ 'bg-none hover:bg-wave-600 active:bg-none border-indigo-700 focus:border-gray-900 focus:shadow-outline-gray' }}@endif border cursor-pointer rounded-md checkout focus:outline-none disabled:opacity-25">
+                        <div data-plan="{{ $plan->plan_id }}" class="inline-flex items-center justify-center mx-auto w-3/4 px-4 py-2 text-base font-semibold text-white transition duration-150 ease-in-out @if($plan->default){{ ' bg-gradient-to-r from-indigo-600 to-indigo-500 hover:from-indigo-500 hover:to-indigo-400' }}@else{{ 'bg-none hover:bg-indigo-600 active:bg-none border-indigo-700 focus:border-none focus:shadow-outline-gray' }}@endif border cursor-pointer rounded-md checkout focus:outline-none disabled:opacity-25">
                             @subscribed($plan->slug)
                                 You are subscribed to this plan
                             @notsubscribed
@@ -58,8 +37,26 @@
                                 @endsubscriber
                             @endsubscribed
                         </div>
-                    </div>
+                </div>
 
+                <div class="relative px-4 pt-0 pb-12 mt-auto text-gray-700 rounded-b-lg">
+
+                    <ul class="flex flex-col space-y-2.5">
+                        @foreach($features as $feature)
+                            <li class="relative">
+                                    <span class="flex items-center">
+                                    <svg class="w-4 h-4 mr-3 text-gray-500 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z"></path>
+                                        <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" transform="translate(4, 0)"></path>
+                                    </svg>
+                                    <span class="text-md text-white dark:text-gray-500">
+                                        {{ $feature }}
+                                    </span>
+                                </span>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
 
--- resources/views/themes/tailwind/pricing.blade.php
+++ resources/views/themes/tailwind/pricing.blade.php
@@ -5,8 +5,8 @@
     <div class="py-20">
         <div class="sm:mx-auto sm:w-full sm:max-w-5xl">
 
-            <h1 class="max-w-md text-4xl font-extrabold text-white sm:mx-auto lg:max-w-none lg:text-5xl sm:text-center">Ready to Get Started?</h1>
-            <p class="max-w-md mx-auto mt-5 text-lg text-gray-500 lg:max-w-none lg:text-xl sm:text-center">Choose a plan tailored to your needs</p>
+            <h1 class="max-w-md text-xl font-normal text-white sm:mx-auto lg:max-w-none lg:text-2xl sm:text-center">Ready to Get Started?</h1>
+            <p class="max-w-md mx-auto mt-4 text-lg text-gray-500 lg:max-w-none lg:text-xl sm:text-center">Choose a plan tailored to your needs</p>
 
         </div>
 
