608527963dc31cd5fd5fe61e1b8c1df5a749a583
nathan shearer <help@rapidstartup.io>
2023-08-01 23:31:52 +0800

    fix footer final

--- resources/views/themes/tailwind/partials/footer.blade.php
+++ resources/views/themes/tailwind/partials/footer.blade.php
@@ -49,7 +49,7 @@
                         <li>
                             <a href="#_" class="flex items-center text-black dark:text-white group">
                             <!-- Icon -->
-                            <span class="w-5 h-5 mr-2 text-gray-600 dark:text-gray-400">
+                            <span class="w-4 h-4 mr-2 text-gray-600 dark:text-gray-400">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
@@ -62,7 +62,7 @@
                         <li>
                             <a href="#_" class="flex items-center text-black dark:text-white group">
                             <!-- Icon -->
-                            <span class="w-5 h-5 mr-2 text-gray-600 dark:text-gray-400">
+                            <span class="w-4 h-4 mr-2 text-gray-600 dark:text-gray-400">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                                 </svg>
@@ -74,7 +74,7 @@
                         <li>
                             <a href="#_" class="flex items-center text-black dark:text-white group">
                             <!-- Icon -->
-                            <span class="w-5 h-5 mr-2 text-gray-600 dark:text-gray-400">
+                            <span class="w-4 h-4 mr-2 text-gray-600 dark:text-gray-400">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                                 </svg>
@@ -151,7 +151,7 @@
                     <h3 class="font-semibold text-black dark:text-white">NEWSLETTER</h3>
                     <ul class="mt-6 space-y-4 text-sm">
                         <li>
-                            <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                            <div class="mt-4 sm:mx-auto sm:w-full sm:max-w-md">
 
                             @if (session('status'))
                                 <div class="p-3 mb-3 text-sm text-indigo-500 bg-indigo-100">
@@ -159,7 +159,7 @@
                                 </div>
                             @endif
 
-                            <div class="border shadow">
+                            <div class="">
                                 <form action="{{ route('password.email') }}" method="POST">
                                     @csrf
                                     <div class="flex">
