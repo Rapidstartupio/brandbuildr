e2fcbc8f348ec551e7415effce698c08a81d7f8d
Mohammed Nasiri <mksnasiri@gmail.com>
2023-08-12 03:47:23 +0100

    fix composer inssue and update openai function

--- app/Http/Controllers/OpenAiController.php
+++ app/Http/Controllers/OpenAiController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use OpenAI\Laravel\Facades\OpenAI;
+use Illuminate\Support\Facades\Http;
 
 class OpenAiController extends Controller
 {
     public function completions(Request $request)
     {
         try {
-            $result = OpenAI::completions()->create($request->all());
-            return response()->json($result['choices'], 200);
+            $url =  "https://api.openai.com/v1/completions";
+            $response = Http::withToken(env('OPENAI_API_KEY'))->post($url, $request->all());
+            return response()->json($response->object()->choices,  $response->status());
         } catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
