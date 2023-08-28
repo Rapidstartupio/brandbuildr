058e9775ad313ae401b2329ac2c866efd6f58bfc
Mohammed Nasiri <mksnasiri@gmail.com>
2023-08-28 22:50:39 +0100

    add projects AI migration tables

---
+++ database/migrations/2023_08_28_210902_create_project_blocks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_blocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->nullable();
+            $table->integer('project_section_id')->unsigned();
+            $table->integer('order')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_blocks');
+    }
+};
---
+++ database/migrations/2023_08_28_210915_create_project_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question_ai');
+            $table->text('question')->nullable();
+            $table->integer('project_block_id')->unsigned();
+            $table->integer('order')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_questions');
+    }
+};
