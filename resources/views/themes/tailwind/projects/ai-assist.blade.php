b30954612e29256f025efabc2b26d61ba664f7e5
Mohammed Nasiri <mksnasiri@gmail.com>
2023-09-05 12:50:54 +0100

    update projects/ai-assist

--- resources/views/themes/tailwind/projects/ai-assist.blade.php
+++ resources/views/themes/tailwind/projects/ai-assist.blade.php
@@ -2,9 +2,46 @@
 
 @section('custom_header_code')
 <style type="text/css">
+    .project-sections .section-title,
+    .project-sections span {
+        display: none;
+    }
+
+    .project-sections .active-section {
+        color: white;
+        display: block;
+    }
+
+    .project-sections .active-section+span,
+    .project-sections .active-section+span+.section-title,
+    .project-sections .active-section+span+.section-title+span,
+    .project-sections .active-section+span+.section-title+span+.section-title {
+        display: block;
+    }
+
+    .project-sections .active-section:before {
+        display: block;
+    }
+
+    .project-sections .section-title:last-of-type+.section-title {
+        background: linear-gradient(90deg, #B6B6B8 0%, rgba(182, 182, 184, 0.00) 119.02%);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+
+    }
+
     .project-sections span:last-child {
         display: none;
     }
+
+    /* .project-sections .section-title:first-of-type {
+        background: linear-gradient(90deg, rgba(182, 182, 184, 0.00) 0%, #B6B6B8 119.02%);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+
+    } */
 </style>
 @endsection
 
@@ -17,10 +54,17 @@
     <div>
         <button type="button" class="focus:outline-none rounded-lg text-gray-900 bg-[#9BDAB4] hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-2     dark:bg-[#9BDAB4] dark:hover:bg-green-700 dark:focus:ring-green-800">RedBull</button>
     </div>
-    <div class="project-sections flex text-center space-x-4 items-center justify-center text-gray-500 pt-6">
-        @foreach($project->type->sections as $section)
-        <div class="text-xl">{{$section->name}}</div> <span class="text-xs">></span>
-        @endforeach
+    <div class="w-full">
+        <div class="project-sections flex text-center space-x-4 items-center justify-center pt-6 whitespace-nowrap truncate" style="color: #B6B6B8;">
+            @foreach($project->type->sections as $section)
+            <div class="section-title @if($section->name == 'Positioning') active-section @endif" style="font-size: 20px;font-family: Helvetica Neue;font-weight: 500;word-wrap: break-word">{{$section->name}}</div> <span class="text-sm">></span>
+            @endforeach
+            @foreach($project->type->sections as $section)
+            <div class="section-title" style="font-size: 20px;font-family: Helvetica Neue;font-weight: 500;word-wrap: break-word">{{$section->name}}</div> <span class="text-sm">></span>
+            @endforeach
+
+
+        </div>
     </div>
 </div>
 
