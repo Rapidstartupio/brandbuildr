0df8a1a4233ee88bdebacd1ad8d5516f46190118
Mohammed Nasiri <mksnasiri@gmail.com>
2023-11-09 22:39:18 +0100

    Add progress spinner to Suggest Button

--- resources/views/themes/tailwind/assets/js/components/Projects/AiAssist.vue
+++ resources/views/themes/tailwind/assets/js/components/Projects/AiAssist.vue
@@ -353,8 +353,14 @@ div.block-item {
                         aria-labelledby="ai-assist-tab"
                     >
                         <div
-                            class="m-4 min-h-[250px] justify-center items-center"
+                            class="m-4 min-h-[250px] justify-center items-center" :class="{'loading-card': isLoading}"
                         >
+
+                            <div role="status" class="absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2" :class="{hidden: !isLoading}">
+                                <svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/><path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/></svg>
+                                <span class="sr-only">Loading...</span>
+                            </div>
+
                             <div>
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -374,7 +380,8 @@ div.block-item {
                                     href="javascript:;"
                                     class="text-base font-medium leading-6 text-gray-500 whitespace-no-wrap border border-white px-12 py-2 text-white rounded-lg hover:text-black hover:bg-white"
                                     v-on:click="showSuggestion()"
-                                >
+                                    :class="{isDisabled : isLoading}" 
+                                    >
                                     Suggest
                                 </a>
                             </div>
@@ -388,12 +395,14 @@ div.block-item {
                                     <button
                                         class="bg-wave-500 hover:bg-wave-700 text-white py-1 px-8 rounded-lg mr-2"
                                         v-on:click="copySuggestionToAnswer()"
+                                        :class="{isDisabled : isLoading}" 
                                     >
                                         Copy to Answer
                                     </button>
                                     <button
                                         class="text-base font-medium leading-6 text-gray-500 whitespace-no-wrap border border-white px-8 py-1 text-white rounded-lg hover:text-black hover:bg-white mr-2 mt-2"
                                         v-on:click="showSuggestion()"
+                                        :class="{isDisabled : isLoading}" 
                                     >
                                         Suggest Again
                                     </button>
@@ -777,6 +786,7 @@ export default {
             // },
             isHiddenSuggestResult: "hidden",
             isSuggest: "",
+            isLoading:false,
             suggestResult: "",
             chatbot: {
                 userInput: "",
@@ -860,6 +870,7 @@ export default {
                 });
         },
         showSuggestion() {
+            this.isLoading = true;
             var prompt = this.steps[this.step].prompt;
             
             // Use regular expressions to replace {{question:X}} with the corresponding value in the $question array
@@ -912,7 +923,10 @@ export default {
             //     "Please answer to this question : " +
             //     this.steps[this.step].question;
             console.log(prompt);
-            if(!prompt) return false;
+            if(!prompt) {
+                this.isLoading = false;
+                return false
+            };
             // switch from completions to chat
                 this.chatbot.previousMessages.push({
                     role: "user",
@@ -939,9 +953,11 @@ export default {
                             this.isHiddenSuggestResult = "";
                             this.steps[this.step].suggest_logs.push(suggestion);
                             //console.log(this.steps[this.step].suggest_logs);
+                            this.isLoading = false;
                         }
                     })
                     .catch((error) => {
+                        this.isLoading = false;
                         console.log(error);
                         console.error("Error fetching bot response:", error);
                     });
