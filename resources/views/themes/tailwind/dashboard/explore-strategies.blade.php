a6d56dcdefa83e9bed54555c13f7bbccd64c4fc8
Mohammed Nasiri <mksnasiri@gmail.com>
2023-11-30 22:30:47 +0100

    update dashboard : add explore strategy

--- resources/views/themes/tailwind/dashboard/explore-strategies.blade.php
+++ resources/views/themes/tailwind/dashboard/explore-strategies.blade.php
@@ -24,7 +24,7 @@
     <div class="md:col-span-3 md:border-l border-gray-400 md:pl-6 md:pl-12">
         <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4  dark:text-white my-5">
             @foreach($projectType as $type)
-            <div class="brandDark2 p-4 rounded relative h-64 @if($type->status == 'disable') contrast-50 cursor-not-allowed @endif">
+            <div class="brandDark2 p-4 rounded relative h-60 @if($type->status == 'disable') contrast-50 cursor-not-allowed @endif">
                 <div class="text-xl capitalize w-min">{{$type->name}}</div>
                 <div class="dark:text-gray-400 text-sm absolute bottom-2 right-2">
                     <svg class="w-auto" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
