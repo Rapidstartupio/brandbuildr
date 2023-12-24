7cfbabcf6a2ba899555bc10ab5b86742391de329
nathan shearer <help@rapidstartup.io>
2023-12-24 15:58:12 +0800

    minor updates from sheet

--- resources/views/themes/tailwind/dashboard/nav.blade.php
+++ resources/views/themes/tailwind/dashboard/nav.blade.php
@@ -1,6 +1,6 @@
 <div class="dark:text-white my-6 dashboard-nav">
     <div class="text-sm font-medium text-center text-gray-500 dark:text-gray-400 md:flex">
-        <ul class="flex flex-wrap -mb-px text-lg">
+        <ul class="flex flex-wrap -mb-px text-lg hidden">
             <li class="mr-10">
                 <a href="{{route('wave.dashboard')}}" class="inline-block  py-2 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 @if(Request::is('dashboard','dashboard/strategy-hub/explore-strategies')) active @endif">Strategy Hub</a>
             </li>
