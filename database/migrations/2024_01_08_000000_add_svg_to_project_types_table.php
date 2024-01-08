1b0395377a05d7203fbae2464c902528c7a1638e
nasirimed <mksnasiri@gmail.com>
2024-01-08 10:12:51 +0100

    Update 2024_01_08_000000_add_svg_to_project_types_table.php

--- database/migrations/2024_01_08_000000_add_svg_to_project_types_table.php
+++ database/migrations/2024_01_08_000000_add_svg_to_project_types_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('project_types, function (Blueprint $table) {
-    //$table->dropColumn('svg');
-});
+    
+        });
     }
 }
