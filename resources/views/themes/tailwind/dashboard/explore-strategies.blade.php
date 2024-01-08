0ab84d6a45efb8cff30656c0b7086eee3c9875a2
nasirimed <mksnasiri@gmail.com>
2024-01-08 10:45:55 +0100

    Update explore-strategies.blade.php

--- resources/views/themes/tailwind/dashboard/explore-strategies.blade.php
+++ resources/views/themes/tailwind/dashboard/explore-strategies.blade.php
@@ -14,10 +14,14 @@
             <div class="p-4 rounded relative h-60 @if($type->status == 'disable') bg-brand-500 disable-type  cursor-not-allowed  @else brandDark3  @endif hover:hover-type  border-2  border-transparent">
                 <!-- cursor-not-allowed  -->
                 <div class="text-xl capitalize w-min">{{$type->name}}</div>
-                <div class="icon dark:text-gray-400 text-sm absolute bottom-2 right-2">
+                <div class="icon dark:text-gray-400 text-sm absolute bottom-2 right-2" width="48" height="48">
+@if($type->svg)
+{!! $type->svg !!}
+@else
                     <svg class="w-auto" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
                         <path d="{{$type->icon_svg_path_d}}" fill="white" />
                     </svg>
+@endif
                 </div>
                 <div class="description brandDark2 contrast-50 hidden absolute bottom-0 left-0">
                     <p class="p-2 text-sm">
