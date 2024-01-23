3c45c341e34f28f2d173222f3ec15d3d2220a05b
nathan shearer <help@rapidstartup.io>
2024-01-23 22:59:30 +0800

    add back button to plans

--- resources/views/themes/tailwind/pricing.blade.php
+++ resources/views/themes/tailwind/pricing.blade.php
@@ -7,6 +7,7 @@
 
         <h1 class="max-w-md text-xl font-normal text-white sm:mx-auto lg:max-w-none lg:text-2xl sm:text-center">Ready to Get Started?</h1>
         <p class="max-w-md mx-auto mt-4 text-lg text-gray-500 lg:max-w-none lg:text-xl sm:text-center">Choose a plan tailored to your needs</p>
+        <p class="max-w-md mx-auto mt-4 text-lg text-gray-500 lg:max-w-none lg:text-xl sm:text-center"><a href="https://brandbuildr.ai/pricing"><< Back to Pricing Options</a></p>
 
     </div>
 
