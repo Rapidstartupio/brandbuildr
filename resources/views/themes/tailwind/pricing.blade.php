0f921f35d0726c9fc9125e8b5149a521fe3c9731
nathan shearer <help@rapidstartup.io>
2024-01-30 01:18:52 +0800

    minor pricing ui tweaks

--- resources/views/themes/tailwind/pricing.blade.php
+++ resources/views/themes/tailwind/pricing.blade.php
@@ -5,9 +5,9 @@
 <div class="py-20 pricing-page">
     <div class="sm:mx-auto sm:w-full sm:max-w-5xl">
 
-        <h1 class="max-w-md text-xl font-normal text-white sm:mx-auto lg:max-w-none lg:text-2xl sm:text-center">Ready to Get Started?</h1>
-        <p class="max-w-md mx-auto mt-4 text-lg text-gray-500 lg:max-w-none lg:text-xl sm:text-center">Choose a plan tailored to your needs</p>
-        <p class="max-w-md mx-auto mt-4 text-lg text-gray-500 lg:max-w-none lg:text-xl sm:text-center wp-plan-page"><a href="https://brandbuildr.ai/pricing"><< Back to Pricing Options</a></p>
+        <h1 class="max-w-md text-xl font-normal text-white mx-auto lg:max-w-none lg:text-2xl text-center">Ready to Get Started?</h1>
+        <p class="max-w-md mx-auto mt-4 text-lg text-gray-500 lg:max-w-none lg:text-xl text-center">Choose a plan tailored to your needs</p>
+        <p class="max-w-md mx-auto mt-4 text-lg text-gray-500 lg:max-w-none lg:text-xl text-center wp-plan-page"><a href="https://brandbuildr.ai/pricing"><< Back to Pricing Options</a></p>
 
     </div>
 
