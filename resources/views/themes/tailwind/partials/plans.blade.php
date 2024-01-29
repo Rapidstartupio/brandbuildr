5669d617d0ba1902b2aaf5b9f06a04fdee4b9b67
nathan shearer <help@rapidstartup.io>
2024-01-29 23:45:06 +0800

    update pricing plan alignment

--- resources/views/themes/tailwind/partials/plans.blade.php
+++ resources/views/themes/tailwind/partials/plans.blade.php
@@ -6,22 +6,22 @@
         <div class="relative flex flex-col h-full mb-10 bg-white dark:bg-brand-950 bg-opacity-15 border @if($plan->default){{ 'border-wave-700' }}@else{{ 'border-none' }}@endif rounded-2xl shadow-xl border-b-none sm:mb-0">
             <div class="@if($plan->default){{ 'border-wave-700' }}@else{{ 'px-4 pt-7' }}@endif">
                 <div> <!-- class="absolute right-0 inline-block transform">-->
-                    <h2 class="relative z-20 w-full h-full px-2 py-1 text-xs leading-tight tracking-wide text-center uppercase border-2 @if($plan->default){{ 'border-none dark:text-white bg-wave-700' }}@else{{ 'hidden border-gray-900 text-gray-800' }}@endif rounded-t">Best value</h2>
+                    <h2 class="relative z-20 w-full h-full px-2 py-1 text-xs leading-tight tracking-wide text-center uppercase border-2 @if($plan->default){{ 'border-none rounded-t-2xl dark:text-white bg-wave-700' }}@else{{ 'hidden border-gray-900 text-gray-800' }}@endif rounded-t">Best value</h2>
                     <!--<span class="absolute -top-7 left-1 w-4 h-4 bg-indigo-700 transform p-2" style="z-index: -1;"></span>
                         <span class="absolute -bottom-7 right-1 w-5 h-4 bg-indigo-700 transform" style="z-index: -1;"></span>-->
                 </div>
             </div>
 
-            <div class="px-5 mt-1">
+            <div class="px-5 mt-2">
                 <span class="plan-name font-hairline dark:text-white text-2xl font-bold">{{ $plan->name }}</span>
             </div>
 
-            <div class="px-5 mt-2 pb-7" style="min-height:110px">
+            <div class="px-5 mt-3 pb-7" style="min-height:110px">
                 <p class="plan-description text-md leading-7 text-gray-300">{!! $plan->description !!}</p>
             </div>
 
-            <div class="px-5">
-                <span class="plan-price font-hairline dark:text-white text-4xl font-bold">${{ $plan->price }}</span>
+            <div class="px-5 mt-5">
+                <span class="plan-price font-hairline dark:text-white text-5xl font-medium">${{ $plan->price }}</span>
                 <span class="per-month text-md font-bold text-gray-500"></span>
             </div>
 
